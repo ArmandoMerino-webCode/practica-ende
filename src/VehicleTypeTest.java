ff0f3cbc9f48890a40565edb966e9ad7f3a61359
ArmandoMerino-webCode <amerino381@gmail.com>
2022-02-20 00:15:25 +0100

    Prueba para hit hub

--- src/VehicleTypeTest.java
+++ src/VehicleTypeTest.java
@@ -3,13 +3,14 @@
 // import org.junit.jupiter.api.Test;
 
 class VehicleTypeTest {
-/**
+/*
 	@Test
 	void test_canBeRentedForMinimumDays1() {
 		VehicleType vehicleType = new VehicleType(4);
 		FechaHora dateTime = new FechaHora();
 		String type = "car";
 		assertEquals(2, vehicleType.canBeRentedForMinimumDays(dateTime, type));
+		Esto es de prueba para hit hub
 	}
 */
 }
